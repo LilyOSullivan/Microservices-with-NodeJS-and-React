0a0564669b32832d75e21aab87531056d9ff2d05
Lily O'Sullivan <lilyosullivan@pm.me>
2022-02-01 22:01:59 +0000

    🚧 Additional NATS testing and updated common module

--- ticketing/nats-test/src/listener.ts
+++ ticketing/nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from "node-nats-streaming";
+import nats from "node-nats-streaming";
 import { randomBytes } from "crypto";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 
 console.clear();
 
@@ -10,27 +11,12 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 stan.on("connect", () => {
 	console.log("Listener connected to NATS");
 
-	const options = stan.subscriptionOptions().setManualAckMode(true);
-	const subscription = stan.subscribe(
-		"ticket:created",
-		"orders-service-queue-group",
-		options
-	);
-
 	stan.on("close", () => {
 		console.log("NATS connection closed!");
 		process.exit();
 	});
 
-	subscription.on("message", (msg: Message) => {
-		const data = msg.getData();
-		if (typeof data === "string") {
-			console.log(
-				`Received event #${msg.getSequence()} with data ${data}`
-			);
-		}
-		msg.ack();
-	});
+	new TicketCreatedListener(stan).listen();
 });
 
 process.on("SIGINT", () => stan.close());
